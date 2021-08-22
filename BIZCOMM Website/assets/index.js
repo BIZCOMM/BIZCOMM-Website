75a41d9b678db844a2069af254dfc3ce02627b24
Vic497 <victor.quocson@gmail.com>
2021-08-23 09:37:41 +1000

    Switchy Switchy text is functional
    poggers

--- BIZCOMM Website/assets/index.js
+++ BIZCOMM Website/assets/index.js
@@ -1,35 +1,3 @@
-var texts = new Array();
-texts.push("[Quick]");
-texts.push("[Creative]");
-texts.push("[Innovative]");
-texts.push("[Elevative]");
-texts.push("[Effective]");
-
-Elevative
-Effective
-Reliable
-Professional
-Efficient
-Cultivative
-Sophisticated
-Systematic
-Productive
-
-var point = 0;
-
-function changeText() {
-    $('#second-text').html(texts[point]);
-    if (point < (texts.length - 1)) {
-        point++;
-    } else {
-        point = 0;
-    }
-
-}
-
-setInterval(changeText, 2000); /*Call it here*/
-changeText();
-
 window.onbeforeunload = function() {
     window.scrollTo(0, 0);
 }
