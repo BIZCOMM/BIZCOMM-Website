123010f5cc294ae5eeebaab639a4e39446f5f083
Jack Tollefsen <53325480+jackUTS@users.noreply.github.com>
2021-09-20 14:57:24 +1000

    Update index.js

--- BIZCOMM Website/assets/index.js
+++ BIZCOMM Website/assets/index.js
@@ -2,3 +2,8 @@ window.onbeforeunload = function() {
     window.scrollTo(0, 0);
 }
 
+var siteWidth = 1280;
+var scale = screen.width /siteWidth;
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+
