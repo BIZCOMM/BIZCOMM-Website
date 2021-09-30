96740ac5ee21ffbd50cdd7693eb0d2374273d500
jackUTS <53325480+jackUTS@users.noreply.github.com>
2021-09-30 15:55:29 +1000

    Updated Navbar

--- BIZCOMM Website/assets/index.js
+++ BIZCOMM Website/assets/index.js
@@ -7,4 +7,6 @@ var scale = screen.width /siteWidth;
 
 document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
 
-
+if (screen.width <= 699) {
+    document.location = "Mobile-Index.php";
+}
